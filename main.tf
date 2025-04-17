ff69498d8b1e7d0d4b15c51ba3ce1fc44fd480a0
William Hearn <william.hearn@ssc-spc.gc.ca>
2025-04-17 08:11:09 -0400

    feat(azurerm): Update for 4.x.x line

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-module "storage_account_name" {
-  source = "git::https://github.com/gccloudone-aurora-iac/terraform-aurora-azure-resource-names-global.git?ref=v2.0.0"
-
-  user_defined = var.user_defined
-}
-
 resource "azurerm_storage_account" "this" {
   name                = module.storage_account_name.storage_account_name
   resource_group_name = var.resource_group_name
